87ce4cba1fe32415ccc78ef22d71b2a08f9c2cdb
xuxueliang <xu.xueliang@163.com>
2019-06-19 16:17:56 +0800

    edit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,7 +3,8 @@ import '../datePicker/datePicker'
 import '../dataPicker/dataPicker'
 @Component({
   tagName: 'my-home',
-  style: require('./home.stylus')
+  style: require('./home.stylus'),
+  shadow: true
 })
 class Home extends Yam {
   $data () {
@@ -40,11 +41,22 @@ class Home extends Yam {
   $mounted () {
     this.$refs.dataPicker.init([[1, 2, 3, 4, 5], [21, 21, 33, 44, 55, 66, 77, 88], [1, 2, 3, 4, 5], [3, 33, 4, 4, 4, 4, 4, 4, 4, 4, 4, 44]])
   }
+  andOther () {
+    return <div>
+      <p>与vue结合</p>
+      <iframe style='width: 100vw;height:50vh' scrolling='no' title='yamjs and vue' src='//codepen.io/xueliang/embed/PrzVOj/?height=265&theme-id=0&default-tab=js,result' frameborder='no' allowtransparency='true' allowfullscreen='true' />
+      <p>与reactjs结合</p>
+      <iframe style='width: 100vw;height:50vh' scrolling='no' title='yamjs and react' src='//codepen.io/xueliang/embed/wLzERM/?height=265&theme-id=0&default-tab=html,result' frameborder='no' allowtransparency='true' allowfullscreen='true'>
+  See the Pen <a href='https://codepen.io/xueliang/pen/wLzERM/'>yamjs and react</a> by Xuxueliang
+  (<a href='https://codepen.io/xueliang'>@xueliang</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>
+    </div>
+  }
   render () {
     return (
       <div>
-        <img src='' />
-        欢迎来到YAMJS
+        <img src='http://192.168.2.154:2521/img?text=YAM-JS&shape=circle&bg=48904e&color=fff' />
+        欢迎
         <div className='mt30' onClick={this.showDete.bind(this)}>点击我唤起日期选择</div>
         <p className='fs-12 c888'>选择一个日期</p>
         <div className='' onClick={this.showDeta.bind(this)}>点击我唤起数据拾起器</div>
@@ -52,8 +64,7 @@ class Home extends Yam {
         <p >{this.date}</p>
         <date-picker showzg change={this.change.bind(this)} ref='datePicker' />
         <data-picker change={this.change.bind(this)} dataChange={this.dataChange.bind(this)} ref='dataPicker' />
-        <p>与vue结合</p>
-        <iframe style='width: 100vw;height:50vh' scrolling='no' title='yamjs and vue' src='//codepen.io/xueliang/embed/PrzVOj/?height=265&theme-id=0&default-tab=js,result' frameborder='no' allowtransparency='true' allowfullscreen='true' />
+        {this.andOther()}
       </div>
     )
   }
